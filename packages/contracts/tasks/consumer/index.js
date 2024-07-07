133ae1e5283df429eef7931679de0991c457ffe9
Buns Enchantress <bunsthedev@gmail.com>
2024-07-07 16:57:47 -0500

    remove tasks: issue, deploy

--- packages/contracts/tasks/consumer/index.js
+++ packages/contracts/tasks/consumer/index.js
@@ -1,5 +1,3 @@
-exports.deployConsumer = require("./deploy.js")
-exports.issueRequest = require("./issue.js")
 exports.readData = require("./read.js")
 exports.requestData = require("./request.js")
 exports.requestHouseInfo = require("./houseInfo.js")
