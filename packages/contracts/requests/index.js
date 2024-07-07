bbf45107f20c70eb38e2847cf2f1949978d1ec8b
Buns Enchantress <bunsthedev@gmail.com>
2024-07-07 18:40:14 -0500

    remove unused request: price, metadata

--- packages/contracts/requests/index.js
+++ packages/contracts/requests/index.js
@@ -1,3 +1,1 @@
-exports.metadataRequest = require("./metadata/index.js");
 exports.pricesRequest = require("./prices/index.js");
-exports.priceRequest = require("./price/config.js");
--- packages/contracts/requests/prices/index.js
+++ packages/contracts/requests/prices/index.js
@@ -8,23 +8,15 @@ const apiResponse = await Functions.makeHttpRequest({
 });
 
 const listPrice = Number(apiResponse.data.listPrice);
-const originalPrice = Number(apiResponse.data.originalPrice);
-const taxValue = Number(apiResponse.data.taxValue);
 
 console.log(`List Price: ${listPrice}`);
-console.log(`Original List Price: ${originalPrice}`);
-console.log(`Tax Assessed Value: ${taxValue}`);
 
 const encoded = abiCoder.encode([
-    `uint256`, 
-    `uint256`, 
-    `uint256`, 
+    `uint256`,
     `uint256`
 ], [
-    tokenId, 
-    listPrice, 
-    originalPrice, 
-    taxValue
+    tokenId,
+    listPrice
 ]);
 
 return ethers.getBytes(encoded);
